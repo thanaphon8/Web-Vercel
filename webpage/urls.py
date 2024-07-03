457ed352e86eba92af1754b67a59947fa44c0692
phirapat0161Blu <ulucky5684@gmail.com>
2024-07-03 13:01:52 +0700

    add new web1

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path("",  views.indexPage),
     path("about/", views.aboutUs),
-    path("contact/", views.contactUs)
+    path("contact/", views.contactUs),
 ]
