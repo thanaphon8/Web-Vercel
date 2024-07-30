b88982d52c882dc551a6dcac0ecc3fa6e465dd04
phirapat0161Blu <ulucky5684@gmail.com>
2024-07-30 11:28:26 +0700

    add home link

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",  views.indexPage),
-    path("about/", views.aboutUs),
+    path("",  views.indexPage, name="index"),
+    path("about/", views.aboutUs, name="about"),
     path("contact/", views.contactUs),
 ]
